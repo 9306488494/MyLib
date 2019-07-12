aa758acefdc30f0dada638151ed80add10d9c4c1
Pradeep Kumar Sheoran <42609516+PradeepSheoran@users.noreply.github.com>
2019-07-12 12:20:59 +0530

    IMEI and Readmore
    Lib1

--- lib/src/main/java/com/example/lib/Lib1.java
+++ lib/src/main/java/com/example/lib/Lib1.java
@@ -4,8 +4,9 @@ import android.widget.TextView;
 
 public class Lib1 {
     // read more
-    public void ReadMore(TextView txtView, String data, String noOfDigit) {
+    public static TextView ReadMore(TextView txtView, String data, String noOfDigit) {
         txtView.setText(data.substring(0, Integer.parseInt(noOfDigit)));
+        return txtView;
     }
 
 }
